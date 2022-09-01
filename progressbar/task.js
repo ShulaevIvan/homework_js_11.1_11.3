3ab6abe2dd8ebbfd30cefd1579c13387b9037c07
Shulaev <mail@ntdservice.ru>
2022-09-01 13:56:30 +0300

    fix_progress_bar

--- progressbar/task.js
+++ progressbar/task.js
@@ -1,18 +1,61 @@
+function sendRequest(data){
+
+  return new Promise((resolve, reject)=>{
+
+    let request = new XMLHttpRequest()
+    let progress = document.querySelector('#progress')
+    let card = document.querySelector('.card')
+    let progressText = document.createElement('p')
+
+    card.appendChild(progressText)
+
+    request.onprogress = (e)=>{
+
+      if (e.loaded){
+        progressText.textContent = `загружено ${e.loaded} байт`
+        progress.setAttribute('value', e.loaded / 100000000)
+      };
+    };
+
+    request.onloadend = ()=> {
+      if (request.status >= 200) {
+
+        progress.setAttribute('value', 1)
+        progressText.remove()
+        resolve('good')
+      }
+      else {
+        reject(this.status)
+      };
+    };
+
+    request.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php')
+    request.send(data)
+  });
+};
+
+
 window.addEventListener('DOMContentLoaded', ()=>{
 
-    let form = document.getElementById('form')
-    let progress = document.getElementById('progress')
-    
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-    
-        let formData = new FormData(form);
-        let request = new XMLHttpRequest();
+  const form = document.querySelector('#form')
+  const btn = document.querySelector('#send')
 
-        request.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php')
-        request.send(formData)
-        request.onprogress = (e) => {
-          progress.value = e.loaded / 10000000
-        };
+  btn.addEventListener('click', (e)=>{
+
+    e.preventDefault()
+    let data = new FormData(form)
+
+    btn.disabled = true
+
+    sendRequest(data)
+    .then((value)=> {
+      console.log(value)
+      btn.disabled = false
+    })
+    .catch((err)=>{
+      console.log(err)
+      btn.disabled = false
+      
     });
+  });
 });
